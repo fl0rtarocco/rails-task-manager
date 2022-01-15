7dd9ac0f29eebb95896d14c1d4313ad079100340
Florencia Tarocco <florenciatarocco@gmail.com>
2022-01-15 12:14:22 +0100

    list all tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'task/:id', to: 'tasks#show'
 end
+
+
+
+# C
+# new
+# create
+# R
+# index
+# show
+# U
+# edit
+# update
+# D
+# destroy
